9e4c579b5d2c0c800e0c21b94daccb65dcd97caa
Denis-isnotavailable <92397311+Denis-isnotavailable@users.noreply.github.com>
2023-03-09 12:26:13 +0200

    Update MainPage.jsx

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -2,13 +2,23 @@ import CharactersList from "components/CharactersList/CharactersList";
 import Search from "components/Search/Search";
 import { LogoBoxStyled, MainPageStyled } from "./MainPage.styled";
 import logoMob from "../../images/logo-mob.png";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import logoDeskTop from "../../images/logo-dt.png"
 
 
 const MainPage = () => {
     // eslint-disable-next-line no-unused-vars
     const [windowHeight, setWindowHeight] = useState(window.innerHeight);
+
+    useEffect(() => { 
+        function handleResize() {
+            setWindowHeight(window.innerHeight)            
+        }
+
+        window.addEventListener('resize', handleResize)
+
+        return _ => { window.removeEventListener('resize', handleResize) };
+    }, []);
 
     return (
         <MainPageStyled>
