4cebe976b296e69f6e3910610862300293301f01
Denis-isnotavailable <92397311+Denis-isnotavailable@users.noreply.github.com>
2023-03-09 12:38:47 +0200

    logo fixed

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,30 +1,15 @@
+
 import CharactersList from "components/CharactersList/CharactersList";
 import Search from "components/Search/Search";
 import { LogoBoxStyled, MainPageStyled } from "./MainPage.styled";
-import logoMob from "../../images/logo-mob.png";
-import { useEffect, useState } from "react";
-import logoDeskTop from "../../images/logo-dt.png"
 
 
-const MainPage = () => {
-    // eslint-disable-next-line no-unused-vars
-    const [windowHeight, setWindowHeight] = useState(window.innerHeight);
 
-    useEffect(() => { 
-        function handleResize() {
-            setWindowHeight(window.innerHeight)            
-        }
-
-        window.addEventListener('resize', handleResize)
-
-        return _ => { window.removeEventListener('resize', handleResize) };
-    }, []);
+const MainPage = () => { 
 
     return (
         <MainPageStyled>
-            <LogoBoxStyled>
-                <img src={windowHeight <= 767 ? logoMob : logoDeskTop} alt="logo" />
-            </LogoBoxStyled>
+            <LogoBoxStyled />
             <Search />
             <CharactersList />
         </MainPageStyled>        
--- src/pages/MainPage/MainPage.styled.jsx
+++ src/pages/MainPage/MainPage.styled.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
-
+import logoMob from "../../images/logo-mob.png";
+import logoDeskTop from "../../images/logo-dt.png";
 
 export const MainPageStyled = styled.div`
     
@@ -7,15 +8,21 @@ export const MainPageStyled = styled.div`
 
 export const LogoBoxStyled = styled.div`
     width: 312px;
+    height: 104px;
     margin-left: auto;
     margin-right: auto;
     margin-top: 92px;
     margin-bottom: 32px;
+    background-image: url(${logoMob});
+    background-repeat: no-repeat;
+    background-size: cover;
 
     @media (min-width: 768px) {
         width: 600px;
+        height: 200px;
         margin-top: 86px;
         margin-bottom: 16px;
+        background-image: url(${logoDeskTop});
     }
 `;
 
